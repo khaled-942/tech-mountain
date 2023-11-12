130a4b6c255c76f3fead39190123f83acbf59cda
khaled <76748059+khaled-942@users.noreply.github.com>
2023-11-12 17:41:18 +0200

    sections

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-document.getElementById('pause-play').addEventListener('click',function(){
-    document.getElementById('carouselExampleCaptions').setAttribute('data-bs-ride','false');
-    this.setAttribute('src','assets/images/play-circle.svg');
-})
+$(".forLinkC").on("click", (e) => {
+  $(e.target).addClass("active");
+  Array.from($(e.target).siblings()).map((e) => {
+    $(e).removeClass("active");
+  });
+  let x = Number($(e.target).attr("data-bs-slide-to"));
+  $("#carouselExampleCaptions3").carousel(x);
+});
+function detect() {
+  if (window.scrollY == 0) {
+    $("#nav").css("background-color", "transparent");
+    $("#logo").attr("src", "assets/images/logo.png");
+  } else {
+    $("#nav").css("background-color", "black");
+    $("#logo").attr("src", "assets/images/logo2.png");
+  }
+}
+detect();
+$(document).on("scroll", detect);
