917f4612d2ca7b88ee65f27d3f6b8b6635404dd9
khaled <76748059+khaled-942@users.noreply.github.com>
2023-11-23 14:30:36 +0200

    langs

---
+++ js/langs.js
@@ -0,0 +1,31 @@
+let swithLangArObject = {
+  Home: {
+    0: "HOME",
+    1: "الصفحة الرئيسية",
+  },
+
+  About: {
+    0: "ABOUT",
+    1: "من نحن",
+  },
+
+  Services: {
+    0: "SERVICES",
+    1: "خدماتنا",
+  },
+
+  Projects: {
+    0: "PROJECTS",
+    1: "المشاريع",
+  },
+
+  Portfolio: {
+    0: "PORTFOLIO",
+    1: "المعرض",
+  },
+
+  Contact: {
+    0: "CONTACT US",
+    1: "للاتصال بنا",
+  },
+};
